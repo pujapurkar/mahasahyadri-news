6d12f7158384153512ae20882eecead46655b4fb
pujapurkar <pujapurkar20@gmail.com>
2026-05-22 22:31:03 +0530

    fix: next config standalone and remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'standalone',
-
   images: {
-    domains: [
-      'localhost',
-      'mahasahyadri-news.onrender.com',
-      'res.cloudinary.com'
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'mahasahyadri-news.onrender.com',
+      },
     ],
   },
 };
