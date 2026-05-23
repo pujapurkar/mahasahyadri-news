ca63cf1e8fe527de768a9ee44d4198d202b2193a
pujapurkar <pujapurkar20@gmail.com>
2026-05-23 22:19:30 +0530

    Fix package.json scripts + next.config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // For file uploads in public/Uploads
-  output: 'standalone',   // ← Add this for Render
-
   images: {
-    domains: ['localhost', 'your-app.onrender.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+      },
+    ],
   },
 };
 
